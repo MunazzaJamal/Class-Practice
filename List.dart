1c9a62760feb653af68d9702a1692766a8b92e4d
MunazzaJamal <munazzahjamal1111@gmail.com>
2024-09-01 12:19:41 +0500

    Modified and updated

---
+++ List.dart
@@ -0,0 +1,5 @@
+//List and arrays are same.
+void main() {
+  List mylist = ["name", 123, true];
+  print(mylist[2]);
+}
--- login_code.dart
+++ login_code.dart
@@ -7,11 +7,13 @@ void main() {
   stdout.write("Enter Password: ");
   String? password = stdin.readLineSync();
 
-  if ((login == "abc@gmail.com") | (login == "abc")) {
-    if (password == "123")
-      print("Login successful");
-    else
-      print("Password incorrect.");
-  } else
-    print("Email/ username is incorrect.");
+  if ((login == "abc@gmail.com") | (login == "abc") && (password == "123"))
+    print("Login successful");
+  else {
+    if ((login != "abc@gmail.com") && (login != "abc") && (password != "123"))
+      print("Email/username and password both incorrect.");
+    else if ((login != "abc@gmail.com") && (login != "abc"))
+      print("Email/ username is incorrect.");
+    else if (password != "123") print("Only Password is incorrect.");
+  }
 }
